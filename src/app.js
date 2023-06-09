5ea9dc2f38264f8f334ea963fcf58d64e252b9f1
MRajczyk <mikolajrajczyk01@gmail.com>
2023-06-09 12:16:37 +0200

    reorganized files, misc changes

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
-    extended: false,
+    extended: true,
   })
 )
 
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-// middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
 })
-// define the home page route
+
 router.get('/', (req, res) => {
   res.send('Hello world!')
 })
-// define the about route
+
 router.get('/about', (req, res) => {
   res.send('about route')
 })
